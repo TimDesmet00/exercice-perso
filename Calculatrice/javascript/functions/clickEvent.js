f8d55ef648ca356d45c8d7114803b0fe55c13704
tim <timdesmet00@gmail.com>
2023-10-23 16:35:02 +0200

    clickevent

---
+++ Calculatrice/javascript/functions/clickEvent.js
@@ -0,0 +1,80 @@
+const one = document.querySelector(".btn1");
+const two = document.querySelector(".btn2");
+const three = document.querySelector(".btn3");
+const four = document.querySelector(".btn4");
+const five = document.querySelector(".btn5");
+const six = document.querySelector(".btn6");
+const seven = document.querySelector(".btn7");
+const eight = document.querySelector(".btn8");
+const nine = document.querySelector(".btn9");
+const zero = document.querySelector(".btn0");
+const clear = document.querySelector("clear");
+const equal = document.querySelector(".equal");
+const plus = document.querySelector(".sum");
+const minus = document.querySelector(".minus");
+const multiply = document.querySelector(".multiply");
+const divide = document.querySelector(".divide");
+
+one.addEventListener("click", () => {
+  console.log("1");
+});
+
+two.addEventListener("click", () => {
+  console.log("2");
+});
+
+three.addEventListener("click", () => {
+  console.log("3");
+});
+
+four.addEventListener("click", () => {
+  console.log("4");
+});
+
+five.addEventListener("click", () => {
+  console.log("5");
+});
+
+six.addEventListener("click", () => {
+  console.log("6");
+});
+
+seven.addEventListener("click", () => {
+  console.log("7");
+});
+
+eight.addEventListener("click", () => {
+  console.log("8");
+});
+
+nine.addEventListener("click", () => {
+  console.log("9");
+});
+
+zero.addEventListener("click", () => {
+  console.log("0");
+});
+
+clear.addEventListener("click", () => {
+  console.log("clear");
+});
+
+equal.addEventListener("click", () => {
+  console.log("=");
+});
+
+plus.addEventListener("click", () => {
+  console.log("+");
+});
+
+minus.addEventListener("click", () => {
+  console.log("-");
+});
+
+multiply.addEventListener("click", () => {
+  console.log("*");
+});
+
+divide.addEventListener("click", () => {
+  console.log("/");
+});
--- Calculatrice/main.js
+++ Calculatrice/main.js
@@ -1,1 +1,2 @@
 import "./sass/style.scss";
+import "./javascript/functions/clickEvent.js";
